ab6fe864c468f3044cc2a78c28f0cf5066c418d3
Pooja <92075579+ppoojjaa@users.noreply.github.com>
2022-08-12 14:08:37 +0530

    Update Ownable.sol

--- Day6/Ownable.sol
+++ Day6/Ownable.sol
@@ -1,7 +1,31 @@
 //SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.4;
 
 contract Ownable{
-    
+    address owner;
+
+    event switchOwners(
+        address indexed prevOwner,
+        address indexed newOwner
+    );
+
+    constructor (){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner{
+        require(msg.sender == owner, "You are not the owner of this contract");
+        _;
+    }
+
+    function transferOwnership(address _adr) external onlyOwner{
+        require(_adr != address(0),"Ownership cannot be transferred to no address!");
+        owner = _adr;
+        emit switchOwners(msg.sender, owner);
+    }
+
+    function checkOwner() public view returns(address){
+        return owner;
+    }
+
 }
